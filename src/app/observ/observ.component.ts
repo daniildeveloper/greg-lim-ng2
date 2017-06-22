fe6761dfcc1162618956691f187311e35c459a8f
daniiltserin <daniilborowkow@yandex.ru>
2017-06-22 17:21:30 +0600

    Observables

--- src/app/observ/observ.component.ts
+++ src/app/observ/observ.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import 'rxjs/add/operator/filter'; // use to filter with value
+import 'rxjs/add/operator/debounceTime'; // use to wait any time between oprations
+import 'rxjs/add/operator/distinctUntilChanged';
 
+/**
+ * Example how to subscribe observables from DOM events. How to contain aplly certain Observable opertors
+ * like filter, debounceTime, distinctUntilChanged to avoid send multiple requests.
+ *
+ * Get obsevable stream from input. 
+ */
 @Component({
   selector: 'app-observ',
   templateUrl: './observ.component.html',
@@ -12,11 +21,15 @@ export class ObservComponent implements OnInit {
    */
   searchControl = new FormControl();
   constructor() {
-    this.searchControl.valueChanges.subscribe(
+    this.searchControl.valueChanges
+      .filter(text => text.length >= 3) // use to filter with value
+      .debounceTime(1000) // use to wait any time between oprations
+      .distinctUntilChanged()
+      .subscribe(
       value => {
         console.log(value);
       }
-    );
+      );
   }
 
   ngOnInit() {
